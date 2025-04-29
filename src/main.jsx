9424c1ffef4f7c5f6c6c90dc6b7914144553dffb
somy <carrepe@naver.com>
2025-04-29 15:40:51 +0900

    queryClient 기본설정

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,14 @@ import { router } from './router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
